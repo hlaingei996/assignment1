cc9b63c17e509313e419c75e401386f8c5235128
hlaingei996 <42799950+hlaingei996@users.noreply.github.com>
2019-01-25 10:07:25 +0630

    Add files via upload

---
+++ assignment_week1/detail.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Main Page</title>
+	<style type="text/css">
+		body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.nav {
+  overflow: hidden;
+  background-color: #007196;
+}
+.nav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+.nav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+.nav a.active {
+  background-color: #435961;
+  color: white;
+}
+.content {
+	margin: 50px;
+	text-align: justify;
+}
+.content a {
+	text-align: center;
+}
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #007196;
+  color: white;
+  text-align: center;
+}
+	</style>
+</head>
+<body>
+	<div class="nav">
+  		<a class="active" href="mainpage.html">Home</a>
+  		<a href="#detail">Detail</a>
+  		<a href="#contact">Contact</a>
+  		<a href="#about">About</a>
+	</div>
+	<div class="content">
+        <section>
+            <header>
+                <h2>CSS Tutorial</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/F1H6VfHl-rw" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </header>       
+        </section>
+    </div>
+    <div class="footer">
+        <p>Posted by: Jenifer</p>
+        <p>Contact information: <a href="mailto:jenifer@gmail.com">
+        jenifer@gmail.com</a>.</p>
+    </div>
+</body>
+</html>
